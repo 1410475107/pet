789499369955bb00df6f2c682c6ee5eab46dfd3a
1410475107 <1410475107@qq.com>
2018-11-10 17:34:52 +0800

    1

--- client/pc/src/components/KwList.vue
+++ client/pc/src/components/KwList.vue
@@ -1,48 +1,44 @@
 <template>
-  <div>
-      <el-form :inline="true"  class="demo-loginForm" label-width="80px">
-        <el-form-item label="关键词：">
-            <el-input name="keywords" v-model="keywords"  class="myinput"></el-input>
-        </el-form-item>
-       
-        <el-form-item>
-            <el-button type="primary" @click="search">查询</el-button>
-        </el-form-item>
-    </el-form>
-
-    <el-table :data="kwlist" border style="width: 100%">
-    <el-table-column prop="id" label="ID" width="60"> </el-table-column>
-    <el-table-column  label="关键词" width="180">
-        <template slot-scope="scope">
-            <div v-html="scope.row.kw"></div>
-       </template>
-    </el-table-column>
-    <el-table-column prop="c1" label="分类一"> </el-table-column>
-    <el-table-column prop="c2" label="分类二"> </el-table-column>
-    <el-table-column prop="c3" label="分类三"> </el-table-column>
-  </el-table>
-    <hr>
-  <el-pagination :page-size="20" :pager-count="11" 
-  layout="prev, pager, next"
-  :total="200000"
-  @current-change = 'getData'
-  >
-</el-pagination>
-
-  </div>
+    <div>
+        <el-form :inline="true" class="demo-loginForm" label-width="80px">
+            <el-form-item label="关键词：">
+                <el-input name="keywords" v-model="keywords" class="myinput"></el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" @click="search">查询</el-button>
+            </el-form-item>
+        </el-form>
+        <el-table :data="kwlist" border style="width: 100%">
+            <el-table-column prop="id" label="ID" width="60"> </el-table-column>
+            <el-table-column label="关键词" width="180">
+                <template slot-scope="scope">
+                    <div v-html="scope.row.kw"></div>
+                </template>
+            </el-table-column>
+            <el-table-column prop="c1" label="分类一"> </el-table-column>
+            <el-table-column prop="c2" label="分类二"> </el-table-column>
+            <el-table-column prop="c3" label="分类三"> </el-table-column>
+        </el-table>
+        <hr>
+        <el-pagination :page-size="20" :pager-count="11" layout="prev, pager, next" :total="200000" @current-change='getData' :current-page="cpage">
+        </el-pagination>
+    </div>
 </template>
 
 <script>
 export default {
-  name: "HelloWorld",
+  name: "KwList",
   data() {
     return {
       kwlist: [],
-      keywords: ""
-    };
+      keywords: "",
+      cpage: 1
+    }
   },
   mounted() {
-    this.serverData(1);
+    //接收get过来的值
+    this.cpage = this.$route.query.page * 1 ? this.$route.query.page * 1 : 1;
+    this.serverData(this.cpage);
   },
   methods: {
     getData(page) {
@@ -64,7 +60,6 @@ export default {
           console.log(res);
           if (_this.keywords) {
             for (const ind in res.data) {
-              console.log(ind);
               res.data[ind].kw = res.data[ind].kw.replace(
                 _this.keywords,
                 `<span class="H">${_this.keywords}</span>`
@@ -72,6 +67,8 @@ export default {
             }
           }
           _this.kwlist = res.data;
+          //修改当前地址 下次可以通过该地址直接访问
+          _this.$router.push({ path: "/list", query: { page: page } });
         })
         .catch(function(error) {
           console.log(error);
